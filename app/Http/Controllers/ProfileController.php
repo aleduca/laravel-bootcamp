1fa87792035891544beada335849fb0498558425
Alexandre Eduardo Cardoso <alecar2007@gmail.com>
2026-01-07 15:07:25 -0300

    Fixed with pint and rector

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,94 +11,94 @@ use Illuminate\View\View;
 
 class ProfileController extends Controller
 {
-	/**
-	 * Display a listing of the resource.
-	 */
-	public function index(): void
-	{
-		//
-	}
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): void
+    {
+        //
+    }
 
-	public function avatar(Request $request, Profile $profile, AvatarService $avatarService)
-	{
-		$request->validate([
-			'avatar' => ['required', 'image', 'mimes:jpg,jpeg,png', 'max:2048'],
-		]);
+    public function avatar(Request $request, Profile $profile, AvatarService $avatarService)
+    {
+        $request->validate([
+            'avatar' => ['required', 'image', 'mimes:jpg,jpeg,png', 'max:2048'],
+        ]);
 
-		$newName = $avatarService->upload(
-			$request->file('avatar'),
-			$profile->avatar,
-			300
-		);
+        $newName = $avatarService->upload(
+            $request->file('avatar'),
+            $profile->avatar,
+            300
+        );
 
-		$profile->avatar = $newName;
-		$profile->save();
+        $profile->avatar = $newName;
+        $profile->save();
 
-		return back()->with('success-avatar', 'Avatar uploaded successfully');
-	}
+        return back()->with('success-avatar', 'Avatar uploaded successfully');
+    }
 
-	/**
-	 * Show the form for creating a new resource.
-	 */
-	public function create(): void
-	{
-		//
-	}
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): void
+    {
+        //
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 */
-	public function store(ProfileRequest $request)
-	{
-		$validated = $request->validated();
-		Profile::create([
-			'user_id' => Auth::id(),
-			...$validated,
-		]);
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ProfileRequest $request)
+    {
+        $validated = $request->validated();
+        Profile::create([
+            'user_id' => Auth::id(),
+            ...$validated,
+        ]);
 
-		return back()->with('success-profile', 'Profile created');
-	}
+        return back()->with('success-profile', 'Profile created');
+    }
 
-	/**
-	 * Display the specified resource.
-	 */
-	public function show(string $id): void
-	{
-		//
-	}
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): void
+    {
+        //
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 */
-	public function edit(): View
-	{
-		$profile = Profile::where('user_id', Auth::id())->first();
-		$isUpdate = false;
-		if ($profile) {
-			$isUpdate = true;
-			$this->authorize('update', $profile);
-		}
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(): View
+    {
+        $profile = Profile::where('user_id', Auth::id())->first();
+        $isUpdate = false;
+        if ($profile) {
+            $isUpdate = true;
+            $this->authorize('update', $profile);
+        }
 
-		return view('profile.edit', compact('profile', 'isUpdate'));
-	}
+        return view('profile.edit', compact('profile', 'isUpdate'));
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 */
-	public function update(ProfileRequest $request, Profile $profile)
-	{
-		$validated = $request->validated();
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ProfileRequest $request, Profile $profile)
+    {
+        $validated = $request->validated();
 
-		$profile->update($validated);
+        $profile->update($validated);
 
-		return back()->with('success-profile', 'Profile updated');
-	}
+        return back()->with('success-profile', 'Profile updated');
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 */
-	public function destroy(string $id): void
-	{
-		//
-	}
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id): void
+    {
+        //
+    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,10 +4,10 @@ namespace App\Providers;
 
 use App\Models\Course;
 use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,11 +25,9 @@ class AppServiceProvider extends ServiceProvider
 	public function boot(): void
 	{
 		foreach (['update-profile', 'update-avatar'] as $limiter) {
-			RateLimiter::for($limiter, function (Request $request) {
-				return Limit::perMinute(3)->by(
-					$request->user()?->id ?: $request->ip()
-				);
-			});
+			RateLimiter::for($limiter, fn (Request $request) => Limit::perMinute(3)->by(
+				$request->user()?->id ?: $request->ip()
+			));
 		}
 
 		Route::bind('course_slug', fn (string $value) => Course::where('slug', $value)->firstOrFail());
